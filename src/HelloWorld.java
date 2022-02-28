93a39cc856f4352c85f64f0e294b55697bac2ce8
Hector Mejia <mejia7hector@gmail.com>
2022-02-28 15:50:45 -0600

    exercise2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,36 @@ public class HelloWorld {
         System.out.print("Hello, ");
         System.out.print("World!");
         System.out.print("\n");
+        int myFavoriteNumber = 99;
+        System.out.println(myFavoriteNumber);
+        String basis = "3.14159";
+        System.out.println(basis);
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+////        int three = (int) o;
+//        int three = (int) "three";
+//        System.out.println(three);
+        int p = 4;
+        p += 5;
+        System.out.println(p);
+
+        int y = 4;
+        int lo = 3;
+        y *= lo;
+        System.out.println(y);
+        int ro = 10;
+        int vo = 2;
+        ro /= vo;
+        System.out.println(ro);
+        vo -= ro;
+        System.out.println(vo);
+        System.out.println(Integer.MAX_VALUE);
     }
 }
+
+
