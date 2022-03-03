18938a8929e90d75546a874bcf65608b3e1129cc
Hector Mejia <mejia7hector@gmail.com>
2022-03-03 14:12:10 -0600

    bob_stuff_java

---
+++ src/exercises/Bob.java
@@ -0,0 +1,22 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("what would you ask Bob?");
+        String userResponse = sc.nextLine().trim();
+        System.out.println(userResponse);
+
+        if (userResponse.endsWith("?")); {
+            System.out.println("Sure");
+        } if (userResponse.endsWith("!")){
+            System.out.println("Woah, chill out!");
+        } else if (userResponse.equals("")){
+            System.out.println("Fine. Bro that way");
+        } else {
+            System.out.println("whatever man");
+        }
+    }
+}
---
+++ src/exercises/StringExercise.java
@@ -0,0 +1,26 @@
+package exercises;
+
+public class StringExercise {
+    public static void main(String[] args) {
+
+//if ("I am a string".equals("I am a string")) {
+//        System.out.println("Strings are equal");
+//        }
+        String first = "We don't need no education \nWe don't need no thought control";
+
+        System.out.println(first);
+
+        String quotes = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(quotes);
+
+        String lore = "In windows, the main drive is usually C:\\";
+        System.out.println(lore);
+
+        String her = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+
+
+
+
+    }
+}
