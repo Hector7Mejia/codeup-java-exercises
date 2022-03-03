5fe16b2f18204b2c9a2f8caa80085a6339bf1eec
Hector Mejia <mejia7hector@gmail.com>
2022-03-03 09:38:47 -0600

    control_flow

--- src/exercises/ConsoleExercises.java
+++ src/exercises/ConsoleExercises.java
@@ -3,13 +3,40 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
-        double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %.2f.", pi);
+//        #1
 
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f.", pi);
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter something: ");
-        String userInput = input.next();
-        System.out.println("You entered: --> \"" + userInput + "\" <--");
-        input.close();
+//        System.out.print("Enter an int: ");
+//        boolean userInput = input.hasNextInt();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        input.close();
+
+
+//#2
+
+//        Scanner answer = new Scanner(System.in);
+        //        System.out.print("Enter 3 words: ");
+//        String userInput = answer.next();
+//        String userInput2 = answer.next();
+//        String userInput3 = answer.next();
+//
+//
+//        System.out.println("Your first word: --> \"" + userInput + "\" <--" + "\nYour second word: --> \"" + userInput2 + "\" <--" + "\nYour second word: --> \"" + userInput3 + "\" <--");
+
+
+//#3
+
+//        System.out.print("Enter a sentence: ");
+//        String userInput = answer.nextLine();
+//
+//        System.out.println("Your sentence: " + userInput);
+
+
+//        #4
+//        input.nextLine();
+//        System.out.println("Enter room length: ");
+//        String lengthString = input.nextLine();
     }
 }
---
+++ src/exercises/ControlFlowExercises.java
@@ -0,0 +1,21 @@
+package exercises;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        int i = 5;
+//        while (i < 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+//        for(int i = 5; i <= 15; i += 1) {
+//
+//            System.out.print(i + " ");
+//        }
+
+        for(int i = 100; i > 0; i -= 5) {
+
+            System.out.println(i);
+        }
+
+    }
+}
