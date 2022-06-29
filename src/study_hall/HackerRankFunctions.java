76b9dc9328f2452342be854e2c91bd21067e96f3
Hector Mejia <mejia7hector@gmail.com>
2022-06-29 14:44:09 -0500

    meetingClass

--- src/study_hall/HackerRankFunctions.java
+++ src/study_hall/HackerRankFunctions.java
@@ -2,19 +2,46 @@ package study_hall;
 
 import org.w3c.dom.Node;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class HackerRankFunctions {
-    public static int diagonalDifference(List<List<Integer>> arr) {
-        // Write your code here
-        int leftdiagonal = 0, rightdiagonal = 0;
-        for(int i = 0, j = arr.get(0).size()-1; i < arr.get(0).size(); i++, j--){
-            leftdiagonal = leftdiagonal + arr.get(i).get(i);
-            rightdiagonal = rightdiagonal + arr.get(i).get(j);
-        }
-        return Math.abs(leftdiagonal - rightdiagonal);
-    }
+    ///*
+    // * Complete the reverseString function
+    // * Use console.log() to print to stdout.
+    // */
+    //function reverseString(s) {
+    //    (typeof s !== 'string')
+    //        ? console.log("s.split is not a function")
+    //        : (s = s.split('').reverse().join(''));
+    //
+    //    console.log(s);
+    //}
+//    function regexVar(str) {
+//  ^ => first item matches:
+// () => stores matching value captured within
+// [aeiou] => matches any of the characters in the brackets
+// . => matches any character:
+// + => for 1 or more occurrances (this ensures str length > 3)
+// \1 => matches to previously stored match.
+        // \2 looks for matched item stored 2 instances ago
+        // \3 looks for matched item stored 3 ago, etc
+
+//  $ ensures that matched item is at end of the sequence
+//
+//        let re = /^([aeiou]).+\1$/;
+//        return re;
+//    }
+//    public static int diagonalDifference(List<List<Integer>> arr) {
+//        // Write your code here
+//        int leftdiagonal = 0, rightdiagonal = 0;
+//        for(int i = 0, j = arr.get(0).size()-1; i < arr.get(0).size(); i++, j--){
+//            leftdiagonal = leftdiagonal + arr.get(i).get(i);
+//            rightdiagonal = rightdiagonal + arr.get(i).get(j);
+//        }
+//        return Math.abs(leftdiagonal - rightdiagonal);
+//    }
 //
 //    public static int height(Node root) {
 //        // Write your code here.
@@ -52,19 +79,61 @@ public class HackerRankFunctions {
 //        int height = height(root);
 //        System.out.println(height);
 //    }
-public static String isBalanced(String s) {
-    // Write your code here
+//public static String isBalanced(String s) {
+//    // Write your code here
+//
+//    int n = -1;
+//    while (s.length() != n) {
+//        n = s.length();
+//        s = s.replace("()", "");
+//        s = s.replace("[]", "");
+//        s = s.replace("{}", "");
+//    }
+//    if (s.length() == 0)
+//        return "YES";
+//    else
+//        return "NO";
+//}
 
-    int n = -1;
-    while (s.length() != n) {
-        n = s.length();
-        s = s.replace("()", "");
-        s = s.replace("[]", "");
-        s = s.replace("{}", "");
-    }
-    if (s.length() == 0)
-        return "YES";
-    else
-        return "NO";
+public static void main(String[] args) {
+//    ArrayList<Integer> runningMedianList = new ArrayList<>();
+//    runningMedianList.add(1);
+//    runningMedianList.add(10);
+//    runningMedianList.add(20);
+//    runningMedian(runningMedianList);
 }
+
+//public static List<Double> runningMedian(List<Integer> a) {
+//    // Write your code here
+//
+//    ArrayList<Double> runningMedianList = new ArrayList<>();
+//
+//    for(int i=1; i < a.size(); i++) {
+//
+//        for (int j = 1; j < a.get(i); j++) {
+//            System.out.println(j);
+//        }
+//
+//    }
+//
+//    return runningMedianList;
+//
+//}
+
+    //javascript//
+    // Declare second integer, double, and String variables.
+    //    var i2;
+    //    var d2;
+    //    var s2;
+    //    // Read and save an integer, double, and String to your variables.
+    //    i2 = parseInt(readLine());
+    //    d2 = parseFloat(readLine());
+    //    s2 = readLine();
+    //    // Print the sum of both integer variables on a new line.
+    //    console.log(i + i2);
+    //    // Print the sum of the double variables on a new line.
+    //    console.log((d + d2).toFixed(1));
+    //    // Concatenate and print the String variables on a new line
+    //    // The 's' variable above should be printed first.
+    //    console.log(s + s2);
 }
---
+++ src/study_hall/Meeting.java
@@ -0,0 +1,119 @@
+package study_hall;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+
+public class Meeting {
+
+    private int startTime;
+    private int endTime;
+
+    public Meeting(int startTime, int endTime) {
+        // number of 30 min blocks past 9:00 am
+        this.startTime = startTime;
+        this.endTime   = endTime;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public static Meeting[] whenEveryonesFree(Meeting[] meetings) {
+
+        // if start time of second is in between start and end date of first meeting.
+        //Meeting(3, 5),Meeting(4, 8)
+
+        Meeting[] scheduledMeetings = new Meeting[meetings.length];
+//        Meeting[] sortedMeetings = new Meeting[meetings.length];
+
+        for (int i = 0; i < meetings.length -1; i++) {
+            if (meetings[i + 1].startTime < meetings[i].startTime && meetings[i + 1].endTime > meetings[i].endTime) {
+
+                scheduledMeetings[i].startTime = meetings[i].startTime;
+                scheduledMeetings[i].endTime = meetings[i + 1].endTime;
+            }
+        }
+
+
+        return meetings;
+    }
+
+    public static List<Meeting> mergeRanges(List<Meeting> meetings) {
+
+        // make a copy so we don't destroy the input
+        List<Meeting> sortedMeetings = new ArrayList<>();
+        for (Meeting meeting: meetings) {
+            Meeting meetingCopy = new Meeting(meeting.getStartTime(), meeting.getEndTime());
+            sortedMeetings.add(meetingCopy);
+        }
+
+        // sort by start time
+        Collections.sort(sortedMeetings, new Comparator<Meeting>() {
+            @Override
+            public int compare(Meeting m1, Meeting m2) {
+                return m1.getStartTime() - m2.getStartTime();
+            }
+        });
+
+        // initialize mergedMeetings with the earliest meeting
+        List<Meeting> mergedMeetings = new ArrayList<>();
+        mergedMeetings.add(sortedMeetings.get(0));
+
+        for (Meeting currentMeeting : sortedMeetings) {
+
+            Meeting lastMergedMeeting = mergedMeetings.get(mergedMeetings.size() - 1);
+
+            // if the current meeting overlaps with the last merged meeting, use the
+            // later end time of the two
+            if (currentMeeting.getStartTime() <= lastMergedMeeting.getEndTime()) {
+                lastMergedMeeting.setEndTime(Math.max(lastMergedMeeting.getEndTime(), currentMeeting.getEndTime()));
+
+                // add the current meeting since it doesn't overlap
+            } else {
+                mergedMeetings.add(currentMeeting);
+            }
+        }
+
+        return mergedMeetings;
+    }
+
+
+    public static void main(String[] args) {
+
+        Meeting[] arrayOfMeetings = new Meeting[5];
+        arrayOfMeetings[0] = new Meeting(0,1);
+        arrayOfMeetings[1] = new Meeting(3,5);
+        arrayOfMeetings[2] = new Meeting(4,8);
+        arrayOfMeetings[3] = new Meeting(10,12);
+        arrayOfMeetings[4] = new Meeting(9,10);
+
+        for (int i = 0; i < arrayOfMeetings.length; i++) {
+            System.out.println(arrayOfMeetings[i].getStartTime());
+        }
+
+        whenEveryonesFree(arrayOfMeetings);
+
+        for (int i = 0; i < arrayOfMeetings.length; i++) {
+            if(arrayOfMeetings[i].getStartTime() ==
+) {
+
+            }
+        }
+
+     }
+}
+
+
