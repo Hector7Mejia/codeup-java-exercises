cad471dfc86dadd3fc77700e62fde115ecc6325d
Hector Mejia <mejia7hector@gmail.com>
2022-07-15 15:33:17 -0500

    reverseString

--- src/study_hall/MapPractice.java
+++ src/study_hall/MapPractice.java
@@ -6,24 +6,39 @@ import java.util.ArrayList;
 public class MapPractice {
 
     int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//    int[] intArrayn = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+    int[] intArrayn = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 //    int[] intArray = new int[20];
 
-   public static void reverseString(int[] toBeReversed) {
+    public static void reverseString(int[] toBeReversed) {
+        ArrayList<Integer> reversedArray = new ArrayList<>();
 
-       ArrayList<Integer> reversedArray = new ArrayList<Integer>();
+        for (int i = toBeReversed.length - 1; i >= 0; i--) {
+            reversedArray.add(toBeReversed[i]);
+        }
+        System.out.println(reversedArray);
+    }
 
-       for(int i=0; i < toBeReversed.length; i--) {
-//           reversedArray = toBeReversed[i];
-
-       }
-   }
+    static void sortit(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+    }
 
     public static void main(String[] args) {
 
-        int[] intArrayn = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+        int[] intArrayn = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 
+        int[] arr = {10, 7, 9, 2, 8, 3, 5, 4, 6, 1, 1};
+        int n = arr.length;
 
+        // for sort an array
+//        sortit(arr, n);
+
+        // for print all the
+        // element in sorted way
+//        for (int i = 0; i < n; i++)
+//            System.out.print(arr[i] + " ");
+//        reverseString(intArrayn);
     }
 }
