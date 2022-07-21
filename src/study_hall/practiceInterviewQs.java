f0fa8fdff73fc3254a32f8db4a4780fe554977b6
Hector Mejia <mejia7hector@gmail.com>
2022-07-21 16:39:34 -0500

    ALMOST_String[]_answer

--- src/study_hall/practiceInterviewQs.java
+++ src/study_hall/practiceInterviewQs.java
@@ -1,14 +1,53 @@
 package study_hall;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class practiceInterviewQs {
+    public static void main(String[] args) {
+        String[] arr = {"Daisy", "Rose", "Hyacinth", "Poppy"};
+
+        bullshitLC(arr);
+
+    }
     //For arr = ["Daisy", "Rose", "Hyacinth", "Poppy"], the output should be solution(arr) = "DRHPaoyoisapsecpyiynth".
+    //Daisy |Rose |Hyacinth| Poppy
+    //  DRHPaoyoisapsecpyiyinth
     //
     //First, we append all 0th characters and obtain string "DRHP";
     //Then we append all 1st characters and obtain string "DRHPaoyo";
     //Then we append all 2nd characters and obtain string "DRHPaoyoisap";
-    //Then we append all 3rd characters and obtain string "DRHPaoyoisapaecp";
+    //Then we append all 3rd characters and obtain string "DRHPaoyoisapsecp";
     //Then we append all 4th characters and obtain string "DRHPaoyoisapaecpyiy";
     //Finally, only letters in the arr[2] are left, so we append the rest characters and get "DRHPaoyoisapaecpyiynth";
+
+    //get the first string of 0th chars. Add the next one the end of the last String result.
+    //remove the first char at 0 then ++ and delete the next char
+    public static void bullshitLC(String[] stringToStrip) {
+        ArrayList<String> a = new ArrayList<>();
+        String bucket;
+        ArrayList<Character> b = new ArrayList<>();
+
+        while(stringToStrip.length != 0) {
+            for (int i = 0; i < stringToStrip.length; i++) {
+
+                for (int j = 0; j < 4; j++) {
+                    if(stringToStrip[j].isEmpty()) {
+                        continue;
+                    }
+                    b.add(stringToStrip[j].charAt(0));
+                    System.out.println(b);
+                    stringToStrip[j] = stringToStrip[j].substring(1);
+                }
+//                System.out.println(Arrays.toString(stringToStrip).);
+            }
+        }
+//        String string = new String(String.valueOf(b));
+//        System.out.println(string);
+//        return bucket;
+    }
+
 
     //Given an array of integers a, your task is to calculate the digits that occur the most number of times in the array. Return the array of these digits in ascending order.
     //
