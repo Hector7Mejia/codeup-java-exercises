9115f7f07860af53b8a169edeb4bb08e059ead11
Hector Mejia <mejia7hector@gmail.com>
2022-07-24 22:23:00 -0500

    ordering int[]

--- src/study_hall/practiceQNums.java
+++ src/study_hall/practiceQNums.java
@@ -1,21 +1,35 @@
 package study_hall;
 
-//public class practiceQNums {
+import java.util.ArrayList;
 
-    //Given an array of integers a, your task is to calculate the digits that occur the most number of times in the array. Return the array of these digits in ascending order.
-    //
-    //Example
-    //
-    //For a = [25, 2, 3, 57, 38, 41], the output should be solution(a) = [2, 3, 5].
+public class practiceQNums {
 
-//public static int[] frequentInts(int[] intArray) {
+//    Given an array of integers a, your task is to calculate the digits that occur the most number of times in the array. Return the array of these digits in ascending order.
 //
-//    intArray
+//    Example
 //
-//}
-//
-//public static void main(String[] args) {
-//    int[] a = {25, 2, 3, 57, 38, 41};
-//    frequentInts(a);
-//}
-//}
+//    For a = [25, 2, 3, 57, 38, 41], the output should be solution(a) = [2, 3, 5].
+
+public static void frequentInts(int[] intArray) {
+
+    ArrayList<Integer> test2 = new ArrayList<Integer>();
+
+
+    for(int i = intArray.length -1; i >= 0; i--){
+        int temp = intArray[i];
+        while(temp>0){
+            test2.add(0, temp%10);  //place low order digit in array
+            temp = temp /10;        //remove low order digit from temp;
+        }
+    }
+    System.out.println(test2);
+
+}
+
+
+
+public static void main(String[] args) {
+    int[] a = {25, 2, 3, 57, 38, 41};
+    frequentInts(a);
+}
+}
