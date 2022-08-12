ee452197758ac92361282546074fa6e8c58297b9
Hector Mejia <mejia7hector@gmail.com>
2022-08-11 19:21:15 -0500

    hashmappin

--- src/study_hall/HackerRank/checkIfIsHacker.java
+++ src/study_hall/HackerRank/checkIfIsHacker.java
@@ -1,5 +1,8 @@
 package study_hall.HackerRank;
 
+import java.util.HashMap;
+import java.util.Locale;
+
 public class checkIfIsHacker {
 private static String checkIfHacker(String s) {
 
@@ -14,5 +17,46 @@ private static String checkIfHacker(String s) {
         }
     }
     return (j == str.length() ? "YES" : "NO");
+}
+
+    public static String pangrams(String s) {
+        // Write your code here
+        int j = 0;
+        String answer = "";
+        String albet = "abcdefghijklmnopqrstuvwxyz";
+        int[] al = new int[albet.length()];
+        String lowercaseS = s.replaceAll("\\s","").toLowerCase();
+        HashMap<Character,Integer> mapBet = new HashMap<>();
+//        System.out.println(lowercaseS);
+        for (int i = 0; i < albet.length(); i++) {
+            al[i] = albet.charAt(i);
+            System.out.println(al[i]);
+        }
+        //if 97-122 present the whole alphabet is thur
+
+//        if(lowercaseS.length() < albet.length()) {
+//            answer = "not pangram";
+//            return answer;
+//        }
+
+        for (int i = 0; i < lowercaseS.length(); i++) {
+            mapBet.put(lowercaseS.charAt(i), i);
+//            System.out.println(i);
+        }
+//        System.out.println(mapBet);
+        for (int i = 0; i < lowercaseS.length(); i++) {
+//            System.out.println(mapBet.containsKey(lowercaseS.charAt(i)));
+//            if(!mapBet.containsKey(albet.charAt(i))) {
+//                answer = "not pangram";
+//                break;
+//
+//            }else{
+                answer = "pangram";
+            }
+        return answer;
+        }
+
+public static void main(String[] args) {
+    System.out.println(pangrams("We promptly judged antique ivory buckles for the next prize"));;
 }
         }
