16372c55fd71fcdfa2c02d804c7f42bf3e51e517
Hector Mejia <mejia7hector@gmail.com>
2022-09-07 14:44:58 -0500

    changeFinished

--- src/study_hall/HackerRank/changeCalc.java
+++ src/study_hall/HackerRank/changeCalc.java
@@ -1,12 +1,28 @@
 package study_hall.HackerRank;
 
 public class changeCalc {
-    public static void smallestChange(double bill) {
+    public static void smallestChange(int bill) {
+        int[] changeArr = {20, 10, 5, 1};
+        String[] change = {"twenties", "tens", "fives", "ones"};
+        int index = 0;
 
+
+        while (bill > 0) {
+            for (int i = 0; i < changeArr.length; i++) {
+                int counter = 0;
+                if(bill >= changeArr[i]) {
+                    counter = bill / changeArr[i];
+                    bill = bill - (counter * changeArr[i]);
+                }
+                System.out.println( "your change in " + change[i] + " is " + counter );
+            }
+        }
     }
 
+
     public static void main(String[] args) {
-        smallestChange(12.12);
+        smallestChange(12);
+//        System.out.println(10 % 12);
     }
 
 }
