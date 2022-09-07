1781ded0e637801afec4220675d7aa02539ff39c
Hector Mejia <mejia7hector@gmail.com>
2022-09-07 09:55:42 -0500

    changeCalc

---
+++ src/study_hall/HackerRank/changeCalc.java
@@ -0,0 +1,12 @@
+package study_hall.HackerRank;
+
+public class changeCalc {
+    public static void smallestChange(double bill) {
+
+    }
+
+    public static void main(String[] args) {
+        smallestChange(12.12);
+    }
+
+}
