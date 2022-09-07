2823a089d7590729718dba57fd90bcd5fc8962ff
Hector Mejia <mejia7hector@gmail.com>
2022-09-06 19:29:03 -0500

    average

--- src/study_hall/HackerRank/findMedian.java
+++ src/study_hall/HackerRank/findMedian.java
@@ -1,17 +1,22 @@
 package study_hall.HackerRank;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class findMedian {
 
-    public long medianCalc(List<Integer> arr) {
+    public static long medianCalc(List<Long> arr) {
         Collections.sort(arr);
-
+        return  arr.get(arr.size() / 2);
     }
 
     public static void main(String[] args) {
-
+        List<Long> oij = new ArrayList<>();
+        for (long i = 0; i < 6; i++) {
+            oij.add(i);
+        }
+        System.out.println(medianCalc(oij));
     }
 }
