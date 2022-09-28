88e213957e639a8fdf5637750763c805340fd7aa
Hector Mejia <mejia7hector@gmail.com>
2022-09-27 22:20:43 -0400

    finished break r

--- src/study_hall/HackerRank/breakingR.java
+++ src/study_hall/HackerRank/breakingR.java
@@ -1,5 +1,6 @@
 package study_hall.HackerRank;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,14 +8,14 @@ public class breakingR {
 
     public static List<Integer> breakingRecords(List<Integer> scores) {
         // Write your code here
-        int maxRecord = scores.get(1);
-        int minRecord = scores.get(1);
+        int maxRecord = scores.get(0);
+        int minRecord = scores.get(0);
         int counter = 0;
         int minCounter = 0;
 
         List<Integer> its = new ArrayList<>();
 
-        for(int i = 1; i < scores.size() -1; i++) {
+        for(int i = 0; i < scores.size(); i++) {
             if(maxRecord < scores.get(i)) {
                 maxRecord = scores.get(i);
                 counter ++;
@@ -30,6 +31,11 @@ public class breakingR {
 
     public static void main(String[] args) {
         List<Integer> ik = new ArrayList<>();
+         int[] low = {3, 4, 21, 36, 10, 28, 35, 5, 24, 42};
+        List<Integer> lo = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            lo.add(low[i]);
+        }
         ik.add(9);
         ik.add(10);
         ik.add(5);
@@ -41,7 +47,7 @@ public class breakingR {
         ik.add(25);
         ik.add(1);
 
-        breakingRecords(ik);
+        System.out.println(breakingRecords(lo));
     }
 
 }
