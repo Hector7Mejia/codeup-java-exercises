d8777924bf79b74e2cc883e5cd93508014b06f97
Hector Mejia <mejia7hector@gmail.com>
2022-09-29 23:56:27 -0400

    records

---
+++ src/study_hall/HackerRank/miniRecords.java
@@ -0,0 +1,24 @@
+package study_hall.HackerRank;
+
+public class miniRecords {
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        //Write your code here
+        int min = scores.get(0);
+        int max = scores.get(0);
+
+        int timesBrokeMin = 0;
+        int timesBrokeMax = 0;
+        for(int i = 1; i < scores.size(); i++) {
+            int score = scores.get(i);
+            if(score > max) {
+                max = score;
+                timesBrokeMax++;
+            } else if(score < min) {
+                min = score;
+                timesBrokeMin++;
+            }
+        }
+
+        return List.of(timesBrokeMax, timesBrokeMin);
+    }
+}
