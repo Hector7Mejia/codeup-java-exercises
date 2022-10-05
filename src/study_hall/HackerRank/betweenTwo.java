770206ac6423f57c9ecf6b4e3e019ca48ba990a7
Hector Mejia <mejia7hector@gmail.com>
2022-10-04 22:06:28 -0400

    second between two sets

--- src/study_hall/HackerRank/betweenTwo.java
+++ src/study_hall/HackerRank/betweenTwo.java
@@ -28,4 +28,47 @@ public class betweenTwo {
         System.out.println(set);
         return set.size();
     }
+    public static int getTotal(List<Integer> a, List<Integer> b) {
+        // Write your code here
+        int outputCounter = 0;
+        Collections.sort(a); Collections.sort(b);
+        int min = a.get(0); int max = b.get(b.size()-1);
+
+        int selectedNumber = min;
+
+        boolean firstArrayCheck = false;
+        boolean secondArrayCheck = false;
+        for (int i = min; i <= max ; i+=1) {
+
+            for (int j = 0; j < a.size(); j++ ) {
+                if ( selectedNumber % a.get(j)  == 0 ) {
+                    // do nothing
+                    firstArrayCheck = true;
+                } else {
+                    firstArrayCheck = false; break;
+                }
+            }
+
+            if (firstArrayCheck) {
+                for (int j = 0; j < b.size(); j++ ) {
+                    if ( b.get(j) % selectedNumber == 0 ) {
+                        // do nothing
+                        secondArrayCheck = true;
+                    } else {
+                        secondArrayCheck = false; break;
+                    }
+                }
+            }
+
+            if (firstArrayCheck && secondArrayCheck) outputCounter++;
+
+            // Resetting ArrayChecks
+            firstArrayCheck = false; secondArrayCheck = false;
+
+            selectedNumber += 1;
+        }
+
+        return outputCounter;
+
+    }
 }
