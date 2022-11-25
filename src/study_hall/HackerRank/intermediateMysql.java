ebe5f7e8dad4980b687b62710cb459138637d04f
Hector Mejia <mejia7hector@gmail.com>
2022-11-24 20:06:47 -0500

    mysql testing

---
+++ src/study_hall/HackerRank/intermediateMysql.java
@@ -0,0 +1,15 @@
+package study_hall.HackerRank;
+
+public class intermediateMysql {
+    //SELECT CONCAT(Name,'(', SUBSTRING(Occupation,1,1) ,')') as Output
+    //FROM OCCUPATIONS
+    //ORDER BY Name;
+    //
+    //SELECT
+    //CONCAT('There are a total of ', num,' ' ,lower(Occupation),'s','.') as Output
+    //FROM (
+    //SELECT Occupation,count(*) as num
+    //FROM OCCUPATIONS
+    //GROUP BY Occupation) AS table1
+    //ORDER BY num, Occupation;
+}
