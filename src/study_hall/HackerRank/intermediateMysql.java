e889c31d7a2a7c04eea80e0a4b86401b23c9cec1
Hector Mejia <mejia7hector@gmail.com>
2022-12-14 21:19:16 -0500

    from where sql

--- src/study_hall/HackerRank/intermediateMysql.java
+++ src/study_hall/HackerRank/intermediateMysql.java
@@ -27,4 +27,14 @@ public class intermediateMysql {
     // SELECT *, 'Leaf' FROM leaf UNION
     // SELECT *, 'Inner' FROM inr
     // order by 1*/
+    //SELECT SUM(POPULATION) FROM CITY WHERE COUNTRYCODE = 'JPN';
+
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
 }
